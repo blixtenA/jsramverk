7ae5c1b071fd2824b655eb53cae2b7202840e260
blixtenA <anbx22@bth.se>
2023-09-13 15:42:39 -0400

    built more tests

--- frontend/test/frontendtest.js
+++ frontend/test/frontendtest.js
@@ -33,13 +33,33 @@ console.log('WebDriver instance created');
 
 const targetURL = "http://localhost:9000";
 
-describe("Sample Test Suite", function () {
+describe("Test Suite", function () {
+
+    function goToNavLink(target) {
+        browser.findElement(By.linkText(target)).then(function(element) {
+            element.click();
+        });
+    }
+
+    function matchUrl(target) {
+        browser.getCurrentUrl().then(function(url) {
+            assert.ok(url.endsWith("multipage/" + target));
+        });
+    }
+
+    function assertH1(target) {
+        browser.findElement(By.css("h1")).then(function(element) {
+            element.getText().then(function(text) {
+                assert.equal(text, target);
+            });
+        });
+    }
+    
   this.timeout(20000);
 
   before(async function () {
     console.log('Launching Chrome...');
     await browser.get(targetURL);
-    console.log('Navigation completed');
     console.log('Current URL:', await browser.getCurrentUrl());
   });
 
@@ -59,6 +79,53 @@ describe("Sample Test Suite", function () {
     
     assert.isTrue(await element.isDisplayed());
   });
+
+  it("Test index", function(done) {
+    let promise = browser.getTitle();
+
+    promise.then(function(title) {
+        assert.equal(title, "Multipage");
+    });
+
+    browser.getTitle().then(function(title) {
+        assert.equal(title, "Multipage");
+    });
+
+    assertH1("Home");
+    matchUrl("#!/");
+
+    done();
+});
+
+it("Test go to Home", function(done) {
+    // try use nav link
+    goToNavLink("Home");
+
+    assertH1("Home");
+    matchUrl("#!/" );
+
+    done();
+});
+
+/* Ticket view specific tests */
+it('should open the ticket view when clicking a delayed item', async function () {
+    // Simulate clicking the first delay (should be at least one, because Sweden)
+    const firstDelayedItem = await browser.findElement(By.css('.train-number'));
+
+    // Execute a click action on the first delayed item
+    await browser.actions().click(firstDelayedItem).perform();
+
+    // Wait for the ticket view container to be visible
+    const ticketViewContainer = await browser.findElement(By.id('container'));
+    await browser.wait(until.elementIsVisible(ticketViewContainer), 20000);
+
+    const ticketTitle = await browser.findElement(By.css('h1'));
+    const backButton = await browser.findElement(By.id('back'));
+
+    // asserts
+    assert.isTrue(await ticketTitle.isDisplayed());
+    assert.isTrue(await backButton.isDisplayed());
+});
   
   after(async function () {
     console.log('Quitting browser');
