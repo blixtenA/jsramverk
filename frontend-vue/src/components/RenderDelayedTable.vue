ecee49668d3dc9ca224ea3cf2924d778ee4625fc
blixtenA <anbx22@bth.se>
2023-09-15 14:47:57 -0400

    Ticket view som modal

--- frontend-vue/src/components/RenderDelayedTable.vue
+++ frontend-vue/src/components/RenderDelayedTable.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div v-for="(item, index) in data" :key="index" @click="renderTicketView(item)">
+    <div v-for="(item, index) in data" :key="index" @click="openTicketView(item)">
+
+      <!-- Display train information -->
       <div class="train-number">{{ item.OperationalTrainNumber }}</div>
       <div class="current-station">
         <div>{{ item.LocationSignature }}</div>
@@ -11,23 +13,46 @@
       </div>
       <div class="delay">{{ outputDelay(item) }}</div>
     </div>
+
+    <!-- Full-page modal -->
+    <div v-if="showTicketView" class="modal">
+      <div class="modal-content">
+        <button @click="closeTicketView">Close Ticket</button>
+        <h1>Nytt ärende #{{ newTicketId }}</h1>
+        <h3 v-if="selectedItem && selectedItem.FromLocation">
+          Tåg från {{ selectedItem.FromLocation[0].LocationName }} till {{ selectedItem.ToLocation[0].LocationName }}. Just nu i {{ selectedItem.LocationSignature }}.
+        </h3>
+        <p v-if="selectedItem"><strong>Försenad:</strong> {{ outputDelay(selectedItem) }}</p>
+        <form @submit.prevent="createTicket">
+          <label for="reason-code">Orsakskod</label><br>
+          <select id="reason-code" v-model="selectedReason">
+            <option v-for="reasonCode in reasonCodes" :key="reasonCode.Code" :value="reasonCode.Code">{{ reasonCode.Level3Description }}</option>
+          </select><br><br>
+          <input type="submit" value="Skapa nytt ärende" />
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import "../../public/style.css";
-
 export default {
+  data() {
+    return {
+      showTicketView: false, // Visa TicketView
+      selectedItem: null, // Item to be displayed in TicketView
+    };
+  },
   props: {
     data: Array,
   },
   methods: {
-    renderTicketView(item) {
-
-      console.log("Selected Item in Parent:", item);
-      console.log("Type of selectedItem:", typeof item);
-
-      this.$router.push({ name: 'ticket', params: { selectedItem: item } });
+    openTicketView(item) {
+      this.selectedItem = item; 
+      this.showTicketView = true;
+    },
+    closeTicketView() {
+      this.showTicketView = false;
     },
     outputDelay(item) {
       const advertised = new Date(item.AdvertisedTimeAtLocation);
@@ -38,3 +63,36 @@ export default {
   },
 };
 </script>
+
+<style>
+.modal {
+  position: fixed;
+  top: 5%; 
+  left: 5%; 
+  width: 90%; 
+  height: 90%; 
+  background-color: rgba(255, 255, 255, 1); 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999; 
+  border: 1px solid #ccc;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); 
+}
+
+.modal-content {
+  padding: 20px; 
+  max-width: 100%; 
+  max-height: 100%; 
+  overflow-y: auto; 
+  z-index: 10000; 
+}
+
+/* Close button */
+.modal-content button {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  padding: 10px 20px;
+}
+
+</style>
